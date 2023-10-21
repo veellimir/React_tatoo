369d2ec9ecc4154225905337a09b854884db28b0
vellimir <kkazancov@gmail.com>
2023-10-21 19:56:08 +0400

    modal wind. for sorting done

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,7 +1,32 @@
+import React from 'react'
+import '../Sort/Sort.css'
+
 function Sort() {
+    const [openModal, setIsVisible] = React.useState(false);
+    const [selected, setSelected] = React.useState(false);
+    const listSort = ['цене', 'цвету', 'размеру']
+
+    const onClickSelect = (i) => {
+        setSelected(i);
+        setIsVisible(false)
+    }
     return (
         <div>
-
+            <div className='wrapper-sorted'>
+                <div className='sorted'>
+                    <b>Сортировка по :</b>
+                    <span onClick={() => setIsVisible( !openModal )}>{listSort[selected]}</span>
+                </div>
+                {openModal && (
+                    <div className='wrapper_select'>
+                        {
+                          listSort.map((name, i) => (
+                            <span key={i} onClick={() => onClickSelect(i)} className={selected === i ? 'active_select' : ''}>{name}</span>
+                          ))  
+                        }
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
